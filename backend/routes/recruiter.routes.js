196ce62fff9bb710f8602f3607123f67dbda718d
Arunravi-255 <arun2551a@gmail.com>
2026-05-16 11:56:54 +0530

    Cleared all starting from beggining

--- backend/routes/recruiter.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const recruiterController = require('../controllers/recruiter.controller');
-
-router.post('/profile',recruiterController.createProfile);
-router.get('/profile/:user_id',recruiterController.getProfile);
-router.post('/job',recruiterController.postJob);
-
-module.exports = router;
--- backend/routes/student.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const studentController = require('../controllers/student.controller');
-
-router.post('/profile',studentController.createProfile);
-router.get('/profile/:user_id',studentController.getProfile);
-router.put('/profile/:user_id',studentController.updateProfile);
-module.exports = router;
